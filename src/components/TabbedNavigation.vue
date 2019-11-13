ecfb15fa8bce1ae7c253ea8303acbc0b56f53370
Levi Zitting <me@levizitting.com>
2019-11-12 19:47:42 -0600

    Use replace prop for tab routes

--- src/components/TabbedNavigation.vue
+++ src/components/TabbedNavigation.vue
@@ -4,22 +4,14 @@
         inline-label
         class="fixed-nav bg-primary text-white shadow-2"
     >
-        <q-route-tab exact name="mails" icon="home" to="/"/>
-        <q-route-tab exact name="contacts" icon="contacts" to="/contacts"/>
-        <q-route-tab exact name="location" icon="where_to_vote" to="/location"/>
-        <q-route-tab exact name="assignment" icon="assignment" to="/assignment"/>
-        <q-route-tab exact name="menu" icon="reorder" to="/menu"/>
+        <q-route-tab exact name="mails" icon="home" to="/" replace />
+        <q-route-tab exact name="contacts" icon="contacts" to="/contacts" replace />
+        <q-route-tab exact name="location" icon="where_to_vote" to="/location" replace />
+        <q-route-tab exact name="assignment" icon="assignment" to="/assignment" replace />
+        <q-route-tab exact name="menu" icon="reorder" to="/menu" replace />
     </q-tabs>
 </template>
 
 <script>
     export default {};
 </script>
-
-<style scoped>
-    /*.fixed-nav {*/
-    /*    position: fixed;*/
-    /*    bottom: 0;*/
-    /*    width: 100%;*/
-    /*}*/
-</style>
